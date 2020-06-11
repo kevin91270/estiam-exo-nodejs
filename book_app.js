bea5c56c957d386fa7f2405dd74740c29f20ecfa
Kévin CHAPELLE <kevin.chapelle@estiam.com>
2020-06-11 09:53:48 +0200

    delete last exo

--- book_app.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-var route_book = require('./book');
-
-app.use('/book', route_book);
-
-app.listen(3000);
--- express.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/', (req, res) =>{
-    res.send('Première application express');
-});
-
-
-app.listen(3000);
